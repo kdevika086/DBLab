c624a29ea6cfa004f58ff8df4f2a0faa3c7497ac
Devika_K <devika.kundathil@gmail.com>
2026-08-12 10:21:36 +0530

    Implemented Stage 2
    - Implement getHeader(), getRecord(), and setRecord()
    - Add multi-block Attribute Catalog traversal using rblock
    - Create Events, Locations, and Participants relations

--- NITCbase/mynitcbase/main.cpp
+++ NITCbase/mynitcbase/main.cpp
@@ -6,32 +6,67 @@
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-  /* Initialize the Run Copy of Disk */
   Disk disk_run;
-  // StaticBuffer buffer;
-  // OpenRelTable cache;
 
-  return FrontendInterface::handleFrontend(argc, argv);
-  unsigned char buffer[BLOCK_SIZE];
+  // create objects for the relation catalog and attribute catalog
+  RecBuffer relCatBuffer(RELCAT_BLOCK);
 
-  //printing hello
-  // Disk::readBlock(buffer, 7000);
-  // char message[] = "hello";
-  // memcpy(buffer + 20, message, 6);
-  // Disk::writeBlock(buffer, 7000);
-  // unsigned char buffer2[BLOCK_SIZE];
-  // char message2[6];
-  // Disk::readBlock(buffer2, 7000);
-  // memcpy(message2, buffer2 + 20, 6);
-  // std::cout << message2 << "\n";
+  HeadInfo relCatHeader;
 
-  //modification
-  // for (int block = 0; block < 4; block++) {
-  //   Disk::readBlock(buffer, block);
-  //   for (int i = 0; i < BLOCK_SIZE; i++) 
-  //   {
-  //     std::cout << (int)buffer[i] << " ";
-  //   }
-  //   std::cout << std::endl;
-  // } 
+  // load the headers of both the blocks into relCatHeader and attrCatHeader.
+  relCatBuffer.getHeader(&relCatHeader);
+
+  for (int i = 0; i < relCatHeader.numEntries; i++) {
+
+    Attribute relCatRecord[RELCAT_NO_ATTRS]; // will store the record from the relation catalog
+
+    relCatBuffer.getRecord(relCatRecord, i);
+
+    printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
+    int currentBlock= ATTRCAT_BLOCK;
+    while(currentBlock !=-1)
+    {
+      RecBuffer attrCatBuffer(currentBlock);
+      HeadInfo attrCatHeader;
+      attrCatBuffer.getHeader(&attrCatHeader);
+
+      //class->batch in students
+      for(int j=0; j<attrCatHeader.numEntries; j++)
+      {
+        Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+        attrCatBuffer.getRecord(attrCatRecord, j);
+
+        if((strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal, "Students") == 0) && 
+          (strcmp(attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, "Class") == 0)) 
+        {
+          strcpy(attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, "Batch");
+          attrCatBuffer.setRecord(attrCatRecord,j);
+          break;
+        }
+      }
+
+      //printing all attributes
+      for (int j = 0; j < attrCatHeader.numEntries; j++) {
+
+        // declare attrCatRecord and load the attribute catalog entry into it
+        Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+        attrCatBuffer.getRecord(attrCatRecord, j);
+
+        if (strcmp(relCatRecord[RELCAT_REL_NAME_INDEX].sVal,
+            attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal) == 0) {
+          const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
+          printf("  %s: %s\n",
+        attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal,
+        attrType);
+        }
+      }
+      currentBlock= attrCatHeader.rblock;
+    }
+    
+    printf("\n");
+  }
+
+  return 0;
 }
+
+
